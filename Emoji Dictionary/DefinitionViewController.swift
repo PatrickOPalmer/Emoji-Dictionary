61c3c340e92bb1b4ae74a0eb9c22688bfb6b795c
Patrick Palmer <dieppe@yahoo.com>
2017-08-11 02:37:50 -0700

    Added definition

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-
+    @IBOutlet weak var definitionLabel: UILabel!
     @IBOutlet weak var emojiLabel: UILabel!
+
     var emoji = "NO EMOJI"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         emojiLabel.text = emoji
+        definitionLabel.text = "Hello"
+
+        if emoji == "😛" {
+            definitionLabel.text = "A smiley with tongue"
+        }
+        if emoji == "🤡" {
+            definitionLabel.text = "Aaaaa! Clown"
+        }
+        if emoji == "💩" {
+            definitionLabel.text = "Poo!"
+        }
+        if emoji == "😱" {
+            definitionLabel.text = "Scared"
+        }
+        if emoji == "👁" {
+            definitionLabel.text = "Eye see you"
+        }
+        if emoji == "☠️" {
+            definitionLabel.text = "Death. Or Poison"
+        }
         // Do any additional setup after loading the view.
     }
 
